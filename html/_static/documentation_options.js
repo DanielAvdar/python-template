814786a9d7609a8085923114470ac460ae0990f2
DanielAvdar <DanielAvdar@users.noreply.github.com>
2025-04-09 12:39:16 +0000

    deploy: 7ee515ed1f8f8cbc411dbe20a784e7f9dd251a02

--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.0.post1.dev0+21f6ad7',
+    VERSION: '0.0.0.post1.dev0+7ee515e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
