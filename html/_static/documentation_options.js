620d5d7841c57760a360352b13086c860f1c5303
DanielAvdar <DanielAvdar@users.noreply.github.com>
2025-04-26 12:03:21 +0000

    deploy: eec19517b590a8260c54878867e65322b90392c5

--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.0.post1.dev0+cc317b8',
+    VERSION: '0.0.0.post1.dev0+eec1951',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
